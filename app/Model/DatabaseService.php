6da7c376ef5adf9e05282189528a09c9901ad1dd
Chien (HCM) <chienlv@magenest.com>
2021-06-09 18:46:17 +0700

    Update

--- app/Model/DatabaseService.php
+++ app/Model/DatabaseService.php
@@ -243,7 +243,7 @@ class DatabaseService
     public function getDownloadUrl()
     {
         global $wpdb;
-        $results = $wpdb->get_results($wpdb->prepare("SELECT id, anime_saved_id, download_url, url FROM " . $this->getDbTableEpisodeName() . " WHERE saved = %d AND is_downloaded = %d LIMIT 1", 1, 0));
+        $results = $wpdb->get_results($wpdb->prepare("SELECT id, anime_saved_id, download_url, url, is_downloaded FROM " . $this->getDbTableEpisodeName() . " WHERE saved = %d AND is_downloaded = %d LIMIT 1", 1, 0));
 
         if(!empty($results)) return $results;
         return null;
